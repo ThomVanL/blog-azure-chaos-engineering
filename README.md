3da326c1c6396262ccf5f143efe8aff420a328c8
Thomas Van Laere <thomasvanlaere@iMacPro.local>
2020-09-30 22:22:31 +0200

    Demo time. 🎉

---
+++ README.md
@@ -0,0 +1,3 @@
+# Azure Chaos Engineering
+
+This is the sample that was used in [my blog post](https://thomasvanlaere.com/posts/2020/09/azure-confidential-computing/) where I decided to take a look at how to apply some Chaos Engineering principles. Since I focus on the Microsoft Azure platform, it only mades sense to have dig a little deeper into [Chaos Toolkit](https://chaostoolkit.org/) and [its extension for Azure](https://github.com/chaostoolkit-incubator/chaostoolkit-azure).
---
+++ azure/scripts/Deploy-AzureSample.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Deploys the sample to Azure.
+
+.DESCRIPTION
+    Deploys the sample to Azure.
+
+.EXAMPLE
+    PS C:\> .\Deploy-AzureSample.ps1
+#>
+[CmdletBinding()]
+param (
+)
+
+###Requires -Modules @{ ModuleName="Az"; ModuleVersion="4.6.1" }
+#Requires -Version 7
+
+if (!(Get-AzContext)) {
+    Connect-AzAccount
+}
+
+$context = Get-AzContext
+if ($context) {
+    $templateFile = Join-Path -Path $PSScriptRoot -ChildPath ".."  -AdditionalChildPath "template", "azuredeploy.json"
+    $templateParametersFile = Join-Path -Path $PSScriptRoot -ChildPath ".."  -AdditionalChildPath "template", "azuredeploy.parameters.json"
+    Write-Host -ForegroundColor Yellow "🚀 Starting subscription level deployment.."
+    New-AzDeployment -Name "thomasvanlaere.com-Blog-Azure-Chaos-Engineering" -TemplateFile $templateFile -TemplateParameterFile $templateParametersFile -Location "westeurope" -Verbose
+
+    $newSp = New-AzADServicePrincipal -Scope "/subscriptions/$($context.Subscription.Id)" -Role "Contributor" -DisplayName "thomasvanlaere.com-Blog-Azure-Chaos-Engineering" -ErrorVariable newSpError
+    if (!$newSpError) {
+        $newSpSecret = ConvertFrom-SecureString -SecureString $newSp.Secret -AsPlainText
+        Write-Host = "Service principal - client ID: $($newSp.ApplicationId)"
+        Write-Host = "Service principal - client secret: $($newSpSecret)"
+    }
+    else {
+        Write-Error "❗️ Could not create service principal!"
+    }
+}
+else {
+    Write-Error -Message "Not connected to Azure."
+}
